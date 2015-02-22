e6a1d4ca9e0a37153bbe3d150d89e860c7c7205c
Michael Brown <mjbrown.droid@gmail.com>
2015-02-22 00:39:20 -0600

    Read existing IP address.

--- wificfg_controller.py
+++ wificfg_controller.py
@@ -3,7 +3,7 @@ from bgapi.bgmodule import BlueGigaClient, GATTService, GATTCharacteristic
 from wificfg_server import UUIDS
 
 def Main():
-    cfg_client = BlueGigaClient(port="COM9", baud=115200, timeout=0.1)
+    cfg_client = BlueGigaClient(port="/dev/ttyACM0", baud=115200, timeout=0.1)
     cfg_client.pipe_logs_to_terminal()
     # BLE Device configuration and start advertising
     cfg_client.reset_ble_state()
@@ -14,7 +14,7 @@ def Main():
         connection.find_information(service=service)
         connection.read_by_type(service=service, type=GATTCharacteristic.CHARACTERISTIC_UUID)
         connection.read_by_type(service=service, type=GATTCharacteristic.CLIENT_CHARACTERISTIC_CONFIG)
-        #connection.read_by_type(service=service, type=GATTCharacteristic.USER_DESCRIPTION)
+        connection.read_by_type(service=service, type=GATTCharacteristic.USER_DESCRIPTION)
 
     app_handles = {}
     for key, value in UUIDS.iteritems():
@@ -24,12 +24,14 @@ def Main():
         if characteristic.has_indicate():
             connection.characteristic_subscription(characteristic=characteristic, indicate=True, notify=False)
 
-    connection.write_by_handle(app_handles["SSID"], "Some SSID", timeout=1)
-    connection.write_by_handle(app_handles["PSK"], "Some PSK", timeout=1)
+    connection.write_by_handle(app_handles["SSID"], "", timeout=1)
+    connection.write_by_handle(app_handles["PSK"], "", timeout=1)
     connection.write_by_handle(app_handles["USERNAME"], "Some Username", timeout=1)
     connection.write_by_handle(app_handles["PASSWORD"], "Some Passowrd", timeout=1)
     connection.write_by_handle(app_handles["ACTIONS"], "\x00\x01", timeout=1)
-    time.sleep(3)
+    connection.read_by_handle(app_handles["ETH0_IP"])
+    connection.read_by_handle(app_handles["WLAN0_IP"])
+    time.sleep(20)
     cfg_client.disconnect(connection)
 
     time.sleep(1)
